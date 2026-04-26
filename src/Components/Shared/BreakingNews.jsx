bb886d50a0ef02a75001bc15a8eeedd468bc44c3
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-04-26 19:05:44 +0600

    add marque

---
+++ src/Components/Shared/BreakingNews.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const BreakingNews = () => {
+    return (
+        <div>
+            <Marquee>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque necessitatibus veniam velit porro quibusdam itaque voluptatibus fugit ut modi unde aliquam, libero, nostrum, dolor ducimus temporibus! Atque odio ab provident?
+            </Marquee>
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -6,6 +6,7 @@ const MainLayout = ({children}) => {
     return (
         <>
          <Header></Header>
+         
         <Navbar></Navbar> 
      <main>   {children} </main>
         </>
